2f08896ad62559df3d5be8aa36c38d9c6f81c779
MohdAmmar01 <mohdammar95822@gmail.com>
2023-06-09 17:11:39 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -5,3 +5,22 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+
+module.exports = {
+  future: {
+    webpack5: true,
+  },
+  webpack(config) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,fs: false, 
+    };
+
+    return config;
+  },
+};
+module.exports = {
+  images: {
+    domains: ["images.unsplash.com"],
+  },
+}
